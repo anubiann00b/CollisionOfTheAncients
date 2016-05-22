6596933bf13d5ccdf2046b3b362db8f530c0779d
Benjamin Yin <fellfromatree@gmail.com>
2016-05-21 18:00:57 -0700

    Added Position Rectangle to StateDataComponent.

--- core/src/me/shreyasr/cota/component/StateDataComponent.scala
+++ core/src/me/shreyasr/cota/component/StateDataComponent.scala
@@ -1,9 +1,9 @@
 package me.shreyasr.cota.component
 
 import com.badlogic.ashley.core.Component
-import me.shreyasr.cota.util.Vec2
+import me.shreyasr.cota.util.{Rectangle, Vec2}
 
-class StateDataComponent(val pos: Vec2 = new Vec2(0, 0)) extends Component {
+class StateDataComponent(val rect:Rectangle, val pos: Vec2 = new Vec2(0, 0)) extends Component {
 
   val lastPos: Vec2 = new Vec2(0, 0)
 
