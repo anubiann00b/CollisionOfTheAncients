11a8b5d3716c76d717960f3460115a82eb690290
Shreyas <skraman1999@gmail.com>
2016-05-21 19:26:40 -0700

    Fix new Vec2 with old functionality

--- core/src/me/shreyasr/cota/util/Vec2.java
+++ core/src/me/shreyasr/cota/util/Vec2.java
@@ -1,21 +1,37 @@
 package me.shreyasr.cota.util;
-public class Vec2{
+public class Vec2 {
+
     public float x;
     public float y;
-    public Vec2(float x, float y){
-        this.x = this.y;
-    }
-    public void add(Vec2 that){
-        x += that.x;
-        y += that.y;
+
+    public Vec2(float x, float y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public void set(Vec2 that){
+    public boolean isZero() {
+        return x == 0 && y == 0;
+    }
+
+    public Vec2 copy() {
+        return new Vec2(x, y);
+    }
+
+    public Vec2 add(Vec2 that) {
+        x += that.x;
+        y += that.y;
+        return this;
+    }
+
+    public Vec2 set(Vec2 that){
         x = that.x;
         y = that.y;
+        return this;
     }
-    public void sub(Vec2 that){
+
+    public Vec2 sub(Vec2 that){
         x -= that.x;
         y -= that.y;
+        return this;
     }
 }
